9b84caa902899406c13ec06f794d76224310b9e1
Akancha <151225526+Akancha0322@users.noreply.github.com>
2024-10-02 20:09:34 +0530

    Time: 27 ms (61.24%), Space: 61.9 MB (63.8%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-    HashMap<Integer,Integer> numToRank = new HashMap<>();
     int sortedArr[] = Arrays.copyOf(arr, arr.length);
     Arrays.sort(sortedArr);
 
     int rank = 1;
+    HashMap<Integer,Integer> numToRank = new HashMap<>();
     for(int i=0; i<sortedArr.length; i++){
         if((i>0) && (sortedArr[i] > sortedArr[i-1])){
             rank++;
@@ -12,12 +12,11 @@ class Solution {
 
         numToRank.put(sortedArr[i], rank);
     }
-
+    
     for(int i=0; i<arr.length; i++){
         arr[i] = numToRank.get(arr[i]);
     }
 
     return arr;
-
     }
 }
