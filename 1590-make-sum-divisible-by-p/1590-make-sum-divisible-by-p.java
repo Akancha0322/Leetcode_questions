87930e1a73b5d7d922ee7651d220c464f3a5530b
Akancha <151225526+Akancha0322@users.noreply.github.com>
2024-10-03 21:25:36 +0530

    Time: 27 ms (39.44%), Space: 57 MB (17.37%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minSubarray(int[] nums, int p) {
+        int n = nums.length;
+        int totalSum = 0;
+
+        for(int i=0; i<n; i++){
+            totalSum = (totalSum + nums[i]) % p; 
+        }
+         
+        int target = totalSum % p;
+        if(target == 0){
+            return 0;
+        }
+
+        HashMap<Integer,Integer> remMap = new HashMap<>();
+        remMap.put(0,-1);
+        int currSum = 0;
+        int minLen = n;
+
+        for(int i=0; i<n; i++){
+            currSum = (currSum + nums[i]) % p;
+            int needed = (currSum - target + p) % p;
+            if(remMap.containsKey(needed)){
+               minLen = Math.min(minLen, i- remMap.get(needed));
+            }
+            remMap.put(currSum , i);
+        }
+
+        if(minLen == n){
+            return -1;
+        }
+
+        return minLen;
+
+    }
+}
